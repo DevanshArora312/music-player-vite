fd5fc480a219b32d196ef2e409d9f0c46500cbf4
DevanshArora312 <devansharora312@gmail.com>
2023-12-19 22:02:52 +0530

    added ytdl-core code

--- server/controllers/getLink.js
+++ server/controllers/getLink.js
@@ -1,45 +1,37 @@
-const ytdl = require("youtube-dl-exec");
+const ytdl = require("ytdl-core");
 
 exports.getLink = async(req,res) => {
     const {link} = req.body;
     // console.log("left"+link+"right");
-    ytdl(link, {
-    dumpSingleJson: true,
-    noCheckCertificates: true,
-    noWarnings: true,
-    preferFreeFormats: true,
-    addHeader: ['referer:youtube.com', 'user-agent:googlebot']
-    })
-    .then(output =>{ 
-        console.log("enters");
-        const check = output;
-        let op = [];
-        output = output.formats;
-        Promise.all(
-            output.map(el => {
-                if (el.resolution === "audio only" && el.format_note == "medium"){
-                    op.push(el);
-                }
-            })
-        )
-        console.log(op);
+    const options = {
+        quality : "highestaudio",
+        filter : "audioonly"
+
+    }
+    try{
+        let op = await ytdl.getInfo(link, options);
+        // console.log(op.videoDetails.thumbnails)
+        const links = []
+        op.formats.map(el => {
+            if (!el.hasVideo && el.hasAudio){
+                links.push(el);
+            }
+        })
+        // console.log(links);
         res.status(200).json({
             ok:true,
             success : true,
-            link : op[op.length - 1].url,
-            thumbnail : check.thumbnail,
-            title : check.title
+            link : links[links.length - 1].url,
+            thumbnail : op.videoDetails.thumbnails[op.videoDetails.thumbnails.length - 1].url,
+            title : op.videoDetails.title
         });
+    } catch(err){
+        res.status(500).json({
+            success:false,
+            message : "Some error in API occured!"
+        })
+    }
         
-    })
-    .catch( err => {
-        res.status(500).json(
-            {
-                ok:false,
-                success:false,
-                data : "Server error occured!",
-                error : err 
-            }
-        )
-    })
+        
+        
 }
