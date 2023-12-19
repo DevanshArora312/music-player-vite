6665e56a7880f37cefc25976e0b0fbfab3090489
DevanshArora312 <devansharora312@gmail.com>
2023-12-19 17:12:12 +0530

    check

--- client/src/Player.jsx
+++ client/src/Player.jsx
@@ -6,7 +6,8 @@ const Player = ({link}) => {
     // const {source,title,thum,err_msg} =  useGetter(link);
     const [playableAudioLink,setPlayableAudioLink] = useState(null);
     useEffect( ()=>{
-        fetch(`${import.meta.env.VITE_REACT_APP_BASE_URL}/get-link`,{method:"POST",headers:{"Content-Type" : "application/json"},body:JSON.stringify({link})})
+        // fetch(`${import.meta.env.VITE_REACT_APP_BASE_URL}/get-link`,{method:"POST",headers:{"Content-Type" : "application/json"},body:JSON.stringify({link})})
+        fetch(`https://music-player-backend-95j9.onrender.com/api/v1/get-link`,{method:"POST",headers:{"Content-Type" : "application/json"},body:JSON.stringify({link})})
             .then(res => {
                 return res.json();
             })
